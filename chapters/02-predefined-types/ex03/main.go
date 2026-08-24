90ece99c1478c9eea5f75e4ca07e60b43b02ce74
Alejandro <aabuezo@gmail.com>
2026-08-24 14:04:19 -0300

    updated ex03

--- chapters/02-predefined-types/ex03/main.go
+++ chapters/02-predefined-types/ex03/main.go
@@ -5,28 +5,33 @@ import (
 )
 
 func main() {
-	s := "Hola, mundo 🌎"
-	fmt.Printf("len(s): %d\n", len(s))
-	fmt.Printf("s: %s\n", s)
+	// len e índices trabajan con bytes, mientras que range sobre un string recorre runes
 
-	for i, v := range s {
-		fmt.Printf("i: %v(type: %T),\tv:%v(type: %T)\tl: %c\n", i, i, v, v, v)
+	s := "Hola, mundo 🌎!"
+	fmt.Println(s)
+
+	// len(s)
+	fmt.Println("len(s):", len(s))
+
+	// recorrido por índice
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%x ", s[i]) // cada byte como hexadecimal
 	}
 	fmt.Println()
 
-	for i := 0; i < len(s); i++ {
-		fmt.Printf("i: %d byte: %v char: %c\n", i, s[i], s[i])
+	// recorrido con range
+	for _, r := range s {
+		fmt.Printf("%c ", r) // como caracter (rune)
 	}
+	fmt.Println()
 
-	bs := []byte(s)
-	fmt.Println(bs)
+	for _, r := range s {
+		fmt.Printf("%v ", r) // como punto de codigo Unicode (rune)
+	}
+	fmt.Println()
 
-	s2 := string(bs)
-	fmt.Println(s2)
-
-	fmt.Println(s == s2) // se pueden comparar con == !
-
-	rs := []rune(s)
-	fmt.Println(rs)
-	fmt.Println(len(rs))
+	// posición e índice de cada carácter
+	for i, r := range s {
+		fmt.Printf("índice: %d, rune: %c, valor: %v\n", i, r, r)
+	}
 }
