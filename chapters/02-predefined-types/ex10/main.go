bf88c494004650cef9c8e1e5e62d31413753b1e4
Alejandro <aabuezo@gmail.com>
2026-08-25 12:48:30 -0300

    updated ex10

--- chapters/02-predefined-types/ex10/main.go
+++ chapters/02-predefined-types/ex10/main.go
@@ -1,4 +1,31 @@
 package main
 
+import "fmt"
+
 func main() {
+	nums := []int{1, 2, 3, 4, 5}
+	a := nums[:2]
+
+	fmt.Println(nums)
+	fmt.Println(a, len(a), cap(a))
+
+	a = append(a, 30)
+	fmt.Println(nums)
+	fmt.Println(a, len(a), cap(a))
+
+	a = append(a, 40)
+	fmt.Println(nums)
+	fmt.Println(a, len(a), cap(a))
+
+	a = append(a, 50)
+	fmt.Println(nums)
+	fmt.Println(a, len(a), cap(a))
+
+	a = append(a, 60) // aca ya no modifica el backing array
+	fmt.Println(nums)
+	fmt.Println(a, len(a), cap(a))
+
+	a = append(a, 70)
+	fmt.Println(nums)
+	fmt.Println(a, len(a), cap(a))
 }
