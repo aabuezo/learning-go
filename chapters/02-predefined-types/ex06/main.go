70e9cce2579a0eb15faae06d43e8a2f9a811e4b3
Alejandro <aabuezo@gmail.com>
2026-08-25 11:49:52 -0300

    updated ex06

--- chapters/02-predefined-types/ex06/main.go
+++ chapters/02-predefined-types/ex06/main.go
@@ -1,4 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
+	arr1 := [3]int{1, 2, 3}
+	arr2 := [3]int{1, 2, 3}
+	arr3 := [4]int{1, 2, 3, 4}
+	arr4 := [4]int{1, 2, 3, 5}
+	fmt.Println(arr1 == arr2) // true
+	// fmt.Println(arr1 == arr3)	// invalid operation: arr1 == arr3 (mismatched types [3]int and [4]int)
+	fmt.Println(arr3 == arr4) // false
 }
