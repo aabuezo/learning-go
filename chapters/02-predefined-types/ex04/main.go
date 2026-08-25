9c93b219f0f38f4fe512b84c50cfd2b7b92fb59a
Alejandro <aabuezo@gmail.com>
2026-08-25 11:44:41 -0300

    updated ex04 and ex05

--- chapters/02-predefined-types/ex04/main.go
+++ chapters/02-predefined-types/ex04/main.go
@@ -1,4 +1,28 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	arr := [5]int{}
+	arr[0] = 1
+	arr[1] = 2
+	arr[2] = 3
+	arr[3] = 4
+	arr[4] = 5
+
+	for i := 0; i < len(arr); i++ {
+		fmt.Print(arr[i], " ")
+	}
+	fmt.Println()
+
+	for i := range arr {
+		fmt.Print(arr[i], " ")
+	}
+	fmt.Println()
+
+	for _, v := range arr {
+		fmt.Print(v, " ")
+	}
+	fmt.Println()
 }
