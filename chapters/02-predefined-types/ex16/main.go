7452ccd7e4b947f83873232dd2e1b7062ab8af44
Alejandro <aabuezo@gmail.com>
2026-08-25 13:55:06 -0300

    updated ex16

--- chapters/02-predefined-types/ex16/main.go
+++ chapters/02-predefined-types/ex16/main.go
@@ -1,4 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
+	ages := map[string]int{
+		"Ana":  30,
+		"Luis": 25,
+		"Juan": 30,
+	}
+	// Crear un nuevo map:
+	byAge := map[int][]string{}
+
+	// Donde la clave sea la edad y el valor sea la lista de nombres con esa edad.
+	for k, v := range ages {
+		byAge[v] = append(byAge[v], k)
+	}
+
+	// Imprimir
+	for k, v := range byAge {
+		fmt.Println(k, v)
+	}
 }
