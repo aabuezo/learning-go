311045ae9a15950092b20c6fc8f684260ad3b54e
Alejandro <aabuezo@gmail.com>
2026-08-25 12:54:52 -0300

    updated ext11

--- chapters/02-predefined-types/ex11/main.go
+++ chapters/02-predefined-types/ex11/main.go
@@ -1,4 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
+	src := []int{1, 2, 3, 4, 5}
+	dst := make([]int, 3)
+
+	fmt.Println("src:", src)
+
+	total := copy(dst, src)
+	fmt.Println("dst:", dst) // [1 2 3]
+	fmt.Println("total elementos copiados:", total)
+
+	dst = make([]int, 10) // crea un slice con 10 elementos 0
+	total = copy(dst, src)
+	fmt.Println("dst:", dst) // [1 2 3 4 5 0 0 0 0 0]
+	fmt.Println("total elementos copiados:", total)
 }
