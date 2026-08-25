831d17cff9a6b0ff7ff6d0c73d1ebc8b4bfe9a30
Alejandro <aabuezo@gmail.com>
2026-08-25 12:20:32 -0300

    updated ex08

--- chapters/02-predefined-types/ex08/main.go
+++ chapters/02-predefined-types/ex08/main.go
@@ -1,4 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
+	s := []int{}
+
+	fmt.Printf("%5s%6s%5s%5s\n", "iter", "value", "len", "cap")
+	for i := 0; i < 20; i++ {
+		s = append(s, i+1)
+		fmt.Printf("%5d%6d%5d%5d\n", i+1, s[i], len(s), cap(s))
+	}
 }
