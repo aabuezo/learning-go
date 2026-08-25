8882696f78e6148bfb366ab97e0d06fe3f7c01d3
Alejandro <aabuezo@gmail.com>
2026-08-25 13:49:41 -0300

    updated ex15

--- chapters/02-predefined-types/ex15/main.go
+++ chapters/02-predefined-types/ex15/main.go
@@ -1,4 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
+	words := []string{"go", "python", "go", "java", "go", "python"}
+	fmt.Println(words)
+
+	// Crear un map que cuente cuantas veces aparece cada palabra.
+	m := map[string]int{}
+
+	for _, w := range words {
+		m[w]++
+	}
+
+	// Imprimir
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
 }
