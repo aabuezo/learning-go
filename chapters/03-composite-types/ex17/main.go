387a39223d1d93410730a6094af0a1e57c1e56d8
Alejandro <aabuezo@gmail.com>
2026-08-27 22:06:26 -0300

    updated chapters/03-composite-types/ext17

--- chapters/03-composite-types/ex17/main.go
+++ chapters/03-composite-types/ex17/main.go
@@ -1,4 +1,30 @@
 package main
 
+type Address struct {
+	Street string
+	City   string
+}
+
+type Customer struct {
+	ID      int
+	Name    string
+	Address Address
+}
+
 func main() {
+
+	address := Address{
+		Street: "123 Main St",
+		City:   "Anytown",
+	}
+
+	customer := Customer{
+		ID:      1,
+		Name:    "John Doe",
+		Address: address,
+	}
+
+	println("Customer ID:", customer.ID)
+	println("Customer Name:", customer.Name)
+	println("Customer Address:", customer.Address.Street, ",", customer.Address.City)
 }
