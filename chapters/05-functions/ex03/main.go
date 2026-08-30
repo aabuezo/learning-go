5733b2627e46eb7ce554d7e6e5dd9e06c877f4a9
Alejandro <aabuezo@gmail.com>
2026-08-30 15:10:56 -0300

    updated chapters/05-functions/ex03

--- chapters/05-functions/ex03/main.go
+++ chapters/05-functions/ex03/main.go
@@ -1,4 +1,16 @@
 package main
 
+import "fmt"
+
+func add(m map[string]int, k string, v int) {
+	m[k] = v
+}
+
 func main() {
+	m := map[string]int{}
+	fmt.Println(m)
+
+	add(m, "hello", 1)
+	add(m, "world", 2)
+	fmt.Println(m)
 }
