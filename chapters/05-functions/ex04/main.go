b97efffe583d891b67119fb9483063dfc92b55a0
Alejandro <aabuezo@gmail.com>
2026-08-30 15:18:28 -0300

    updated chapters/05-functions/ex04

--- chapters/05-functions/ex04/main.go
+++ chapters/05-functions/ex04/main.go
@@ -1,4 +1,32 @@
 package main
 
+import "fmt"
+
+type User struct {
+	Name string
+	Age  int
+}
+
+func updateCopy(user User, name string) {
+	user.Name = name
+	fmt.Println("updateCopy:", user)
+}
+
+func updateGood(user *User, name string) {
+	user.Name = name
+	fmt.Println("updateGood", *user)
+}
+
 func main() {
+	u := User{
+		Name: "john",
+		Age:  43,
+	}
+	fmt.Println("main:", u)
+
+	updateCopy(u, "jane")
+	fmt.Println("main:", u)
+
+	updateGood(&u, "lali")
+	fmt.Println("main:", u)
 }
