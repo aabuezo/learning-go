1203c55dde580fbd6b15e8cf0b7812490708451e
Alejandro <aabuezo@gmail.com>
2026-08-30 15:07:45 -0300

    updated chapters/05-functions/ex02

--- chapters/05-functions/ex02/main.go
+++ chapters/05-functions/ex02/main.go
@@ -1,4 +1,40 @@
 package main
 
+import (
+	"fmt"
+)
+
+func add(lst []int, val int) {
+	lst = append(lst, val)
+}
+
+func addRet(lst []int, val int) []int {
+	lst = append(lst, val)
+	return lst
+}
+
 func main() {
+	vals := []int{}
+
+	for i := range 3 {
+		add(vals, i+1)
+		fmt.Printf("%v, len: %d, cap: %d\n", vals, len(vals), cap(vals))
+	}
+
+	for i := range 3 {
+		vals = addRet(vals, i+1)
+		fmt.Printf("vals: %v, len: %d, cap: %d\n", vals, len(vals), cap(vals))
+	}
+
+	vals = make([]int, 0, 10)
+
+	for i := range 3 {
+		add(vals, i+1)
+		fmt.Printf("%v, len: %d, cap: %d\n", vals, len(vals), cap(vals))
+	}
+
+	for i := range 3 {
+		vals = addRet(vals, i+1)
+		fmt.Printf("vals: %v, len: %d, cap: %d\n", vals, len(vals), cap(vals))
+	}
 }
