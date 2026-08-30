d3452562b68163db787a81b7d90642c646a3f599
Alejandro <aabuezo@gmail.com>
2026-08-30 14:49:44 -0300

    updated chapters/05-functions/ex01

--- chapters/05-functions/ex01/main.go
+++ chapters/05-functions/ex01/main.go
@@ -1,4 +1,41 @@
 package main
 
+import "fmt"
+
+func updateSlice(lst []int, idx, val int) error {
+	if idx < 0 || idx >= len(lst) {
+		return fmt.Errorf("invalid index: %d", idx)
+	}
+
+	lst[idx] = val
+	return nil
+}
+
 func main() {
+	vals := []int{1, 2, 3, 4, 5}
+	fmt.Println(vals)
+
+	if err := updateSlice(vals, -1, 10); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(vals)
+	}
+
+	if err := updateSlice(vals, 0, 10); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(vals)
+	}
+
+	if err := updateSlice(vals, len(vals), 60); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(vals)
+	}
+
+	if err := updateSlice(vals, len(vals)-1, 60); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(vals)
+	}
 }
