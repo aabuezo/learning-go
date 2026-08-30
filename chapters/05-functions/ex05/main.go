117567bda3a82a3fd36516d0d1429a3802550d99
Alejandro <aabuezo@gmail.com>
2026-08-30 15:47:31 -0300

    updated chapters/05-functions/ex05

--- chapters/05-functions/ex05/main.go
+++ chapters/05-functions/ex05/main.go
@@ -1,4 +1,28 @@
 package main
 
+import "fmt"
+
+func ops(a, b int) (int, int, int, error) {
+	if b == 0 {
+		return 0, 0, 0, fmt.Errorf("b no puede ser cero")
+	}
+	return a + b, a - b, a / b, nil
+}
+
 func main() {
+	a, b := 10, 5
+	suma, resta, division, err := ops(a, b)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("suma:", suma)
+	fmt.Println("resta:", resta)
+	fmt.Println("division:", division)
+
+	a, b = 10, 0
+	_, _, _, err = ops(a, b)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
