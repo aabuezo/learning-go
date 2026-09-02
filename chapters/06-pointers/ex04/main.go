1d703250a17a15e9038a573d2cec39b9be1ee62f
Alejandro <aabuezo@gmail.com>
2026-09-02 15:20:35 -0300

    updated chapters/06-pointers/ex04

--- chapters/06-pointers/ex04/main.go
+++ chapters/06-pointers/ex04/main.go
@@ -1,4 +1,27 @@
 package main
 
+import "fmt"
+
+type User struct {
+	Name string
+	Age  int
+}
+
+func updateUser(u User, name string) {
+	u.Name = name
+}
+
 func main() {
+	u := User{Name: "John", Age: 23}
+	pu := &u
+	fmt.Println(u)
+
+	pu.Name = "Jane" // syntactic sugar para (*pu).Name
+	fmt.Println(u)
+
+	(*pu).Name = "Patrick"
+	fmt.Println(u)
+
+	updateUser(u, "John")
+	fmt.Println(u) // {Patrick 23}
 }
