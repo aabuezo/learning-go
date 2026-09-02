cdc9dd8560f8a9949e1876524112a6c710e056e5
Alejandro <aabuezo@gmail.com>
2026-09-02 13:27:32 -0300

    updated chapters/05-functions/ex08

--- chapters/05-functions/ex08/main.go
+++ chapters/05-functions/ex08/main.go
@@ -1,4 +1,22 @@
 package main
 
+func crearContador() func() int {
+	count := 0
+	return func() int { // closure
+		count++
+		return count
+	}
+}
+
 func main() {
+
+	count1 := crearContador()
+	println(count1()) // 1
+	println(count1()) // 2
+	println(count1()) // 3
+
+	count2 := crearContador()
+	println(count2()) // 1
+	println(count2()) // 2
+	println(count2()) // 3
 }
