e49d8b23ad7fa47c7eb1eed0feeee5e4be4c98f4
Alejandro <aabuezo@gmail.com>
2026-09-02 14:50:05 -0300

    updated chapters/06-pointers/ex02

--- chapters/06-pointers/ex02/main.go
+++ chapters/06-pointers/ex02/main.go
@@ -1,4 +1,30 @@
 package main
 
+import "fmt"
+
+// increment recibe la dirección de i y modifica el valor original.
+func increment(i *int) {
+	if i == nil {
+		return
+	}
+	*i++
+}
+
+// decrement recibe una copia de i; su modificación se pierde al terminar.
+func decrement(i int) {
+	i--
+}
+
 func main() {
+	i := 42
+	fmt.Println(i) // 42
+
+	increment(&i) // modifica la variable i
+
+	fmt.Println(i) // 43
+
+	decrement(i) // no modifica la variable i
+
+	fmt.Println(i) // 43
+
 }
