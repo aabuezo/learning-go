ea1c40fd11e455b6863ad37ebcc3047aba2baf9a
Alejandro <aabuezo@gmail.com>
2026-09-02 14:44:14 -0300

    updated chapters/06-pointers/ex01

--- chapters/06-pointers/ex01/main.go
+++ chapters/06-pointers/ex01/main.go
@@ -1,4 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
+	i := 42
+	p := &i         // p apunta a i (contiene la direccion de memoria de i)
+	fmt.Println(i)  // imprime 42
+	fmt.Println(*p) // imprime 42
+	fmt.Println(p)  // imprime la direccion de i
+
+	*p++ // incrementa el valor de i a través del puntero p
+
+	fmt.Println(i)  // imprime 43
+	fmt.Println(*p) // imprime 43
+	fmt.Println(p)  // imprime la direccion de i
 }
